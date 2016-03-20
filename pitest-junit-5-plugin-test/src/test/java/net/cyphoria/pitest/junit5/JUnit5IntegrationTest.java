8e65e6d9b0586d5888cdf81a27426c5ef57d08c3
Stefan <stefan@cyphoria.net>
2016-03-20 19:16:04 +0100

    Dummy Plugin Factory (fails due to connection reset - must investigate)

---
+++ pitest-junit-5-plugin-test/src/test/java/net/cyphoria/pitest/junit5/JUnit5IntegrationTest.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2016 Stefan Penndorf
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations under the License.
+ */
+package net.cyphoria.pitest.junit5;
+
+import org.apache.maven.it.VerificationException;
+import org.apache.maven.it.Verifier;
+import org.apache.maven.it.util.ResourceExtractor;
+import org.junit.gen5.api.AfterEach;
+import org.junit.gen5.api.BeforeEach;
+import org.junit.gen5.api.Test;
+import org.junit.gen5.api.TestInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+/**
+ * @author Stefan Pennndorf
+ */
+class JUnit5IntegrationTest {
+
+    private static final String VERSION = "1.1.9";
+
+    private static Logger LOGGER = LoggerFactory.getLogger(JUnit5IntegrationTest.class);
+
+    private long startTime;
+
+    private Verifier verifier;
+
+    @BeforeEach
+    void beforeEachTest(final TestInfo testInfo) {
+        LOGGER.info("running test '{}'", testInfo.getDisplayName());
+        startTime = System.currentTimeMillis();
+    }
+
+    @AfterEach
+    void afterEachTest(final TestInfo testInfo) {
+        LOGGER.info("duration of test '{}' {}ms", testInfo.getDisplayName(),
+                System.currentTimeMillis() - startTime);
+    }
+
+    @Test
+    void mutatestSimpleCodeEndExecutesTest() throws IOException, VerificationException {
+        prepare("/simple");
+
+        verifier.executeGoal("org.pitest:pitest-maven:mutationCoverage");
+    }
+
+
+    private File prepare(String testPath) throws IOException, VerificationException {
+
+        Path testFolder = Files.createTempDirectory("pit");
+        String path = ResourceExtractor.extractResourcePath(getClass(), testPath,
+                testFolder.toFile(), true).getAbsolutePath();
+
+        verifier = new Verifier(path);
+        verifier.setAutoclean(false);
+        verifier.setDebug(true);
+        verifier.getCliOptions().add("-Dpit.version=" + VERSION);
+        verifier.getCliOptions().add(
+                "-Dthreads=" + (Runtime.getRuntime().availableProcessors()));
+
+        return new File(testFolder.toAbsolutePath().toFile(), testPath);
+    }
+
+}
---
+++ pitest-junit-5-plugin/src/main/java/net/cyphoria/pitest/junit5/JUnit5TestPluginFactory.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2016 Stefan Penndorf
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations under the License.
+ */
+package net.cyphoria.pitest.junit5;
+
+import org.pitest.classinfo.ClassByteArraySource;
+import org.pitest.classinfo.ClassInfo;
+import org.pitest.functional.Option;
+import org.pitest.help.PitHelpError;
+import org.pitest.testapi.Configuration;
+import org.pitest.testapi.TestClassIdentifier;
+import org.pitest.testapi.TestGroupConfig;
+import org.pitest.testapi.TestPluginFactory;
+import org.pitest.testapi.TestSuiteFinder;
+import org.pitest.testapi.TestUnitFinder;
+
+import java.util.Collections;
+
+/**
+ * @author Stefan Pennndorf
+ */
+public class JUnit5TestPluginFactory implements TestPluginFactory {
+    @Override
+    public Configuration createTestFrameworkConfiguration(TestGroupConfig testGroupConfig, ClassByteArraySource classByteArraySource) {
+//         throw new RuntimeException("Igor was here");
+        return new Configuration() {
+            @Override
+            public TestUnitFinder testUnitFinder() {
+                return aClass -> Collections.emptyList();
+            }
+
+            @Override
+            public TestSuiteFinder testSuiteFinder() {
+                return aClass -> Collections.emptyList();
+            }
+
+            @Override
+            public TestClassIdentifier testClassIdentifier() {
+                return new TestClassIdentifier() {
+                    @Override
+                    public boolean isATestClass(ClassInfo classInfo) {
+                        return false;
+                    }
+
+                    @Override
+                    public boolean isIncluded(ClassInfo classInfo) {
+                        return false;
+                    }
+                };
+            }
+
+            @Override
+            public Option<PitHelpError> verifyEnvironment() {
+                return Option.none();
+            }
+        };
+    }
+
+    @Override
+    public String description() {
+        return "A Test Discovery Plugin for JUnit 5";
+    }
+}
